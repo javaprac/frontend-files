68378c37cfc79804a3d30067a8d7b8b06e4b56b5
Arnab Mondal <arnab2807@gmail.com>
2016-04-10 06:44:56 +0530

    Clean up

--- repository/DataBaseService/src/com/db/service/handler/UpdateHandlerInterface.java
+++
@@ -1,6 +0,0 @@
-package com.db.service.handler;
-
-public interface UpdateHandlerInterface {
-
-	public abstract String getQuery();
-}
--- repository/LoginRegistration/src/com/login/handler/BaseHandler.java
+++
@@ -1,8 +0,0 @@
-package com.login.handler;
-
-import java.util.Map;
-
-public interface BaseHandler {
-	public String getSQL();
-	public Map<String, String> getParamMap(Object obj);
-}
